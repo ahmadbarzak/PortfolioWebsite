d00ab43d9922bb32fcf6386f7898638c8b93f984
ahmadbarzak <76200661+ahmadbarzak@users.noreply.github.com>
2024-05-09 16:11:18 +1200

    removed iframe

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,20 +1,20 @@
 
-// import { motion } from "framer-motion";
-// import styles from '../../Styles/Projects/ProjectCard.module.css';
+import { motion } from "framer-motion";
+import styles from '../../Styles/Projects/ProjectCard.module.css';
 
 const ProjectCard = () => {
 
     return (
 
-        // <motion.div className={styles.card}
-        // initial={{scale:0.5, opacity: 0}}
-        // animate={{scale: 1, opacity: 1}}
-        // transition={{delay:0.6, duration:0.2}}>
-        //     <span>Insert Project Here!</span>
-        // </motion.div>
+        <motion.div className={styles.card}
+        initial={{scale:0.5, opacity: 0}}
+        animate={{scale: 1, opacity: 1}}
+        transition={{delay:0.6, duration:0.2}}>
+            <span>Insert Project Here!</span>
+        </motion.div>
 
-        <iframe style={{width: "800px", height:"500px", border:"5px solid white"}} src="https://ahmadbarzak.com/">
-        </iframe>
+        // <iframe style={{width: "800px", height:"500px", border:"5px solid white"}} src="https://ahmadbarzak.com/">
+        // </iframe>
 
 
     )
